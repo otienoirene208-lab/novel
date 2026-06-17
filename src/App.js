670f56b8d3eda609eb4ed7ac88c9b6f7cf090212
otienoirene208-lab <otienoirene208@gmail.com>
2026-06-17 11:42:13 +0300

    addproducts

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+
+import Signup from './component/Signup';
+import Signin from './component/Signin';
+import Notfound from './component/Notfound';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import Makepayment from './component/Makepayment';
+import Navbar from './component/Navbar';
+import Footer from './component/Footer';
+import Mycarousel from './component/Carousel';
+import Getproduct from './component/Getproducts';
+import Addproduct from './component/Addproducts';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
+      <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Novel sell online</h1>
       </header>
+
+       {/* the navigation component */}
+       <Navbar/>
+
+        {/* carousel components] */}
+        <Mycarousel/>
+
+      <Routes>
+        <Route path='/' element={<Getproduct/>}/>
+        <Route path='/signup'element={<Signup/>}/>
+        <Route path='/signin'element={<Signin/>}/>
+        <Route path='/addproduct'element={<Addproduct/>}/>
+        <Route path='/makepayment'element={<Makepayment/>}/>
+        <Route path='*'element={<Notfound/>}/>
+        
+      </Routes>
+
+        {/* footer component */}
+     <Footer/>
+
     </div>
+    </Router>
   );
 }
 
---
+++ src/component/Carousel.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+const Mycarousel = () => {
+    return (
+        <div id='carouselExample' className='carousel slide row justify-content-center' data-bs-ride='carousel'>
+
+       {/* indicators fot navigation */}
+       <div className='carousel-indicators'>
+        <button type='button' data-bs-target='#carouselExample' data-bs-slide-to="0" className='active'></button>
+        <button type='button' data-bs-target='#carouselExample' data-bs-slide-to="1"></button>
+        <button type='button' data-bs-target='#carouselExample' data-bs-slide-to="2"></button>
+       </div>
+
+            {/* slides */}
+            <div className='carousel-inner'>
+                <div className='carousel-item active'>
+                    <img src="images/images.jpeg" className='d-block w-100' height='200px' alt="" />
+                    
+                </div>
+                <div className='carousel-item'>
+                    <img src="/images/long.jpeg"className='d-block w-100' height='200px' alt="" />
+                   
+                </div>
+                <div className='carousel-item'>
+                    <img src="/image/snow.jpeg" className='d-block w-100' height='200px'alt="" />
+                    
+                </div>
+            </div>
+        {/* controls */}
+        <button className='carousel-control-prev' type='button' data-bs-target="#carouselExample" data-bs-slide="prev">
+            <span className='carousel-control-prev-icon bg-warning' type='button' data-bs-target="#carouselExample" data-bs-slide="next" ></span>
+        </button>
+        <button className='carousel-control-next' data-bs-target="#carouselExample" data-bs-slide="next"> 
+            <span className='carousel-control-next-icon bg-warning'></span>
+        </button>
+
+        <button>
+            <span></span>
+        </button>
+
+        {/* Hello */}
+        {/* Hello */}
+
+
+        </div>
+    )
+}
+
+export default Mycarousel
